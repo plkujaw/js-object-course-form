91d8a8c31a8ea42ddb298e5788919e9f8ed181bd
plkujaw <jawor.jakub@gmail.com>
2020-07-13 10:36:21 +0100

    display constructor function

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// IIFE - Immediately Invoked Function Expression
+
+(function() {
+// check if fields are filled in and hide the submit button
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
+
+// display contructor function
+function Display() {
+  this.name = document.getElementById("name");
+  this.course = document.getElementById("course");
+  this.author = document.getElementById("author");
+  this.customers = document.querySelector(".customer-list");
+}
+
+})();
