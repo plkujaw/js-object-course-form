d9f35b6428858ca528d316dddf3d6b52d4ff7b46
plkujaw <jawor.jakub@gmail.com>
2020-07-13 10:53:26 +0100

    checkFields and validateFields

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 // IIFE - Immediately Invoked Function Expression
 
 (function() {
-// check if fields are filled in and hide the submit button
+  // check if fields are filled in and hide the submit button
+  document.addEventListener("DOMContentLoaded", function() {
+    const display = new Display();
+    display.checkFields();
+  });
 
-document.addEventListener("DOMContentLoaded", function() {
+  // display constructor function
+  function Display() {
+    this.name = document.getElementById("name");
+    this.course = document.getElementById("course");
+    this.author = document.getElementById("author");
+    this.customers = document.querySelector(".customer-list");
+  };
 
-});
+  // check fields
+  Display.prototype.checkFields = function() {
+    console.log(this);
+    this.name.addEventListener("blur", this.validateField);
+    this.course.addEventListener("blur", this.validateField);
+    this.author.addEventListener("blur", this.validateField);
+  };
 
-// display contructor function
-function Display() {
-  this.name = document.getElementById("name");
-  this.course = document.getElementById("course");
-  this.author = document.getElementById("author");
-  this.customers = document.querySelector(".customer-list");
-}
+  // validate each field
+  Display.prototype.validateField = function() {
+    console.log(this);
+  };
+
 
 })();
